260d31805fae7bd32eb4e3a9a2b15a47ce431775
socopoko <59167401+socopoko@users.noreply.github.com>
2020-02-09 15:54:04 +0530

    Admin Menu Pages

---
+++ Application Code/ongoingForm.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ongoing Form</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+</head>
+<body>
+<div class="myForm">
+	<h1>Ongoing Placements Entry Form</h1><br><br><br><br>
+	<form action="connect.php" method="post">
+		<h2>Company ID</h2>
+		<input type="number" name="companyID">
+		<h2>Company Name</h2>
+		<input type="text" name="companyName">
+		<h2>Starting Date</h2>
+		<input type="text" name="startDate">
+		<h2>Ending Date</h2>
+		<input type="text" name="endDate">
+		<br>
+		<input type="submit" name="submit">
+	</form>
+</div>
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
